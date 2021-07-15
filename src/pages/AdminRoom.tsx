423ab46c1e74c1d4de7f7e548092dba4303a0bd8
Jessica Zimmerhansl <jlzimmerhansl@gmail.com>
2021-07-15 18:43:46 -0300

    NLW finished

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -6,6 +6,8 @@ import { RoomCode } from '../components/RoomCode';
 import { Question } from '../components/Question';
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 
 import '../styles/room.scss';
 import { database } from '../services/firebase';
@@ -38,6 +40,18 @@ export function AdminRoom() {
     }
   }
 
+  async function handleCheckQuestionAsAnswred(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
+  }
+
   return (
     <div className="page-room">
       <header>
@@ -64,7 +78,32 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        handleCheckQuestionAsAnswred(question.id);
+                      }}
+                    >
+                      <img
+                        src={checkImg}
+                        alt="Marcar pergunta como respondida"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        handleHighlightQuestion(question.id);
+                      }}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta" />
+                    </button>
+                  </>
+                )}
                 <button
                   type="button"
                   onClick={() => {
